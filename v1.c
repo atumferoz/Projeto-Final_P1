c44e8d10e2f42d2393060e9a2cb00d268e0c2daa
HelioBrito671 <80326470+HelioBrito671@users.noreply.github.com>
2024-05-31 15:38:40 +0100

    Criar tarefa
    Criação da função tarefa e difinição das principais estruturas

--- v1.c
+++ v1.c
@@ -1,6 +1,60 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
+#include <jansson.h>
 
-#define MAX 100
+#define DESC 300
 
+
+typedef struct Tarefa {
+  int id;
+  char *descricao;
+  int prioridade; // 1: alto, 0: baixo
+  struct tm dataCriacao;
+  struct tm dataConclusao;
+  int estado; // 0: em espera, 1: em execução, 2: concluída, 3: falha
+  char *payloadJSON;
+} Tarefa;
+
+
+
+typedef struct Fila {
+  Tarefa *inicio, *fim;
+  int tamanho;
+} Fila;
+
+typedef struct Pilha {
+  Tarefa *topo;
+  int tamanho;
+} Pilha;
+
+
+
+void criarTarefa(Tarefa *tarefa) {
+  // Alocar memória para a estrutura Tarefa
+  tarefa = malloc(sizeof(Tarefa));
+
+  if (!tarefa) {
+    printf("Erro ao alocar memória para a tarefa!\n");
+    return;
+  }
+
+  // Solicitar e armazenar os dados da tarefa do usuário
+  printf("Digite o ID da tarefa: ");
+  scanf("%d", &tarefa->id);
+
+  printf("Digite a descrição da tarefa: ");
+  scanf(" %[^\n]", tarefa->descricao);
+
+  printf("Digite a prioridade da tarefa (1 - alto, 0 - baixo): ");
+  scanf("%d", &tarefa->prioridade);
+
+}
+	
+	
+	
+	main()
+	{
+		
+	}
